e29b2d1b31a7e6b431fa9cf13e4ecf5b40a5f94b
Elena Sivogolovko <elena.sivogolovko@arcadia.spb.ru>
2018-06-01 18:00:10 +0300

    server

--- server.py
+++ server.py
@@ -1,18 +1,35 @@
-from flask import Flask
+from flask import Flask, json, render_template, request
 import requests
+
+import locale
+
+import settings
 
 app=Flask(__name__)
 
+locale.setlocale(locale.LC_ALL, "russian")
+json_data = {}
+
 @app.route("/")
 def index():
-    #headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-    #r = requests.get('http://api.data.mos.ru/v1/datasets/2009/rows', headers=headers)
-    session = requests.Session()
-    response = session.get('http://api.data.mos.ru/v1/datasets/2009/rows', headers={'User-Agent': 'Mozilla/5.0'})
+    json_data = get_names_data()
+    return json_data
 
-    print(response)
-    print(response.headers)
-    return "hi"
+@app.route("/names")
+def names():
+    year = request.args.get('year', type = int)
+    print(year)
+    json_data = get_names_data(year)
+    return render_template('table.html', data=json_data)
+
+def get_names_data(year=None):
+    info = {'api_key': settings.API_KEY}
+    if (year):
+        info["$filter"] = "Cells/Year eq {}".format(str(year))
+#2009
+    response = requests.get(settings.FEMALE_SET, params=info)
+    return response.json()
+
 
 if __name__ == '__main__':
     app.run()
